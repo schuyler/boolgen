68896f0e2cfc072d51da983b86327126f08207da
Schuyler Erle <schuyler@lattice.com>
2024-05-27 08:14:08 -0700

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='boolgen',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     description='A library for generating and simplifying Boolean expressions from truth tables',
     long_description=open('README.md').read(),
